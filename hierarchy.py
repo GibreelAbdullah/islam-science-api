6981fb50f74a68ef0253e29dca7ed78ca131685f
GibreelAbdullah <gibreel.khan@gmail.com>
2025-06-09 11:00:49 +0530

    Updated Hierarchy

--- hierarchy.py
+++ hierarchy.py
@@ -2,29 +2,35 @@ import os
 import json
 from bs4 import BeautifulSoup
 
-def generate_file_structure(path):
-    def build_tree(directory, ignoreTopHTML=False):
-        tree = {"name": os.path.basename(directory)}
-        if os.path.isdir(directory):
-            tree["children"] = []
-            for item in os.listdir(directory):
-                item_path = os.path.join(directory, item)
-                if os.path.isdir(item_path):
-                    tree["children"].append(build_tree(item_path))
-                elif item.endswith(".html") and not (ignoreTopHTML and directory == path):
-                    tree["children"].append(build_tree(item_path))
-        else:
-            text = ""
-            with open(directory, "r", encoding="utf-8") as html_file:
-                soup = BeautifulSoup(html_file, "html.parser")
-                first_p = soup.find("p")
-                text = first_p.get_text(strip=True) if first_p else ""
-            tree = {"name": text}
-        return tree
-    return build_tree(path, True)
+def get_directory_structure(rootdir):
+    structure = {}
+    
+    # Get all first-level directories
+    for dir_name in os.listdir(rootdir):
+        dir_path = os.path.join(rootdir, dir_name)
+        if os.path.isdir(dir_path):
+            structure[dir_name] = []
+            
+            # Get all second-level subdirectories
+            for subdir_name in os.listdir(dir_path):
+                subdir_path = os.path.join(dir_path, subdir_name)
+                if os.path.isdir(subdir_path):
+                    structure[dir_name].append(subdir_name)
+                elif subdir_path.endswith(".html"):
+                    text = ""
+                    with open(subdir_path, "r", encoding="utf-8") as html_file:
+                        soup = BeautifulSoup(html_file, "html.parser")
+                        first_p = soup.find("p")
+                        text = first_p.get_text(strip=True) if first_p else ""
+                        structure[dir_name].append(text)
+    return structure
 
 if __name__ == "__main__":
-    current_folder = os.getcwd()
-    file_structure = generate_file_structure(os.path.join(current_folder, "islam-science"))
+    # Get the current directory or specify your root directory
+    root_directory = os.getcwd()
+    
+    # Get the directory structure
+    dir_structure = get_directory_structure(os.path.join(root_directory, "islam-science"))
+
     with open("hierarchy.json", "w") as json_file:
-        json.dump(file_structure, json_file, indent=None)
+        json.dump(dir_structure, json_file, indent=None)
