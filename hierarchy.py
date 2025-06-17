444d06acdcf72e0d25e9b0acb7c82585c92a44f1
GibreelAbdullah <gibreel.khan@gmail.com>
2025-06-17 21:53:45 +0530

    Updated hierarchy

--- hierarchy.py
+++ hierarchy.py
@@ -4,26 +4,32 @@ from bs4 import BeautifulSoup
 
 def get_directory_structure(rootdir):
     structure = {}
-    
-    # Get all first-level directories
-    for dir_name in os.listdir(rootdir):
-        dir_path = os.path.join(rootdir, dir_name)
-        if os.path.isdir(dir_path) and dir_name != "images":
-            structure[dir_name] = []
+    files_list = []
+    with open(os.path.join(rootdir, "FRONTPAGE.html"), "r", encoding="utf-8") as html_file:
+        soup = BeautifulSoup(html_file, "html.parser")
+        grid_div = soup.find("div", class_="grid")
+        links = grid_div.find_all("a") if grid_div else []
+        for link in links:
+            text = link.get_text(strip=True)
+            structure[text] = []
+            href = link.get("href", "")
+            href = href.replace("/islam-science/", "", 1) + ".html"
+            files_list.append({"href": href, "title": text})
             
-            # Get all second-level subdirectories
-            for subdir_name in os.listdir(dir_path):
-                subdir_path = os.path.join(dir_path, subdir_name)
-                if os.path.isdir(subdir_path):
-                    structure[dir_name].append(subdir_name)
-                elif subdir_path.endswith(".html"):
-                    text = ""
-                    with open(subdir_path, "r", encoding="utf-8") as html_file:
-                        soup = BeautifulSoup(html_file, "html.parser")
-                        first_p = soup.find("p")
-                        text = first_p.get_text(strip=True) if first_p else ""
-                        rel_path = os.path.relpath(subdir_path, rootdir)
-                        structure[dir_name].append({"key": text, "url": rel_path})
+    for dir_name in files_list:
+        dir_path = os.path.join(rootdir, dir_name["href"])
+        if dir_path.endswith(".html") and dir_name["href"] != ".html":
+            text = ""
+            with open(dir_path, "r", encoding="utf-8") as html_file:
+                soup = BeautifulSoup(html_file, "html.parser")
+                grid_div = soup.find("div", class_="grid")
+                links = grid_div.find_all("a") if grid_div else []
+                first_p = soup.find("p")
+
+                for link in links:
+                    text = link.get_text(strip=True)
+                    rel_path = link.get("href", "")
+                    structure[dir_name["title"]].append({"key": text, "url": rel_path})
     return structure
 
 if __name__ == "__main__":
